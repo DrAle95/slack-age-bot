3ce5a7a4627a6bbf8c0706b3daecab43c7d888b2
Alexey Lebedev <alexeylebedev1995@gmail.com>
2024-02-04 21:21:39 +0200

    Example is missing field somewhy

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/shomali11/slacker"
+)
+
+func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
+	for event := range analyticsChannel {
+		fmt.Println("Command Events")
+		fmt.Println(event.Timestamp)
+		fmt.Println(event.Command)
+		fmt.Println(event.Parameters)
+		fmt.Println(event.Event)
+		fmt.Println()
+	}
+}
+
+func main() {
+	os.Setenv("SLAKC_BOT_TOKEN", "xxxxx-xxxxx")
+	os.Setenv("SLACK_APP_TOKEN", "xxxxx-xxxxx")
+
+	bot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+
+	go printCommandEvents(bot.CommandEvents())
+
+	bot.Command("My Yob is to <year>", &slacker.CommandDefinition{
+		Description: "yob calculator",
+		Example:     "my Yob is 2020",
+		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
+			year := request.Param("year")
+			yob, err := strconv.Atoi(year)
+
+			if err != nil {
+				fmt.Println("error")
+			}
+			age := 2024 - yob
+			r := fmt.Sprintf("Age is %d", age)
+			response.Reply(r)
+		},
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	err := bot.Listen(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
